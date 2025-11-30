463e7ebf09153792c695e77f07b86d7728cfca09
unknown <rahulcr2797@gmail.com>
2025-11-30 14:57:34 +0530

    completed

--- Employee_Payroll_Management_System.sql
+++ Employee_Payroll_Management_System.sql
@@ -19,12 +19,13 @@ create table employees (
 						Name varchar(50) not null,
 						Department varchar(50) not null,
 						Email varchar(50) unique not null,
-						Phone_No bigint,
+						Phone_No bigint unique,
 						Joining_date date not null,
 						Salary decimal(10,2) not null check (Salary >= 0),
-						Bonus decimal(10,2) not null check (Salary >= 0),
-						Tax_Percentage decimal(5,2) DEFAULT 0 CHECK (tax_percentage BETWEEN 0 AND 100)
+						Bonus decimal(10,2) not null check (Bonus >= 0),
+						Tax_Percentage decimal(5,2) DEFAULT 0 CHECK (Tax_Percentage BETWEEN 0 AND 100)
 );
+
 
 
 
@@ -41,7 +42,7 @@ insert into employees (Name, Department, Email, Phone_no, Joining_date, Salary, 
 				('Olivia Taylor', 'HR', 'olivia.taylor@example.com', 9789012345, '2025-03-05', 68000.00, 3000.00, 10.0),
 				('James Anderson', 'Sales', 'james.anderson@example.com', 9567890123, '2024-07-14', 90000.00, 10000.00, 12.5),
 				('Ava Thomas', 'IT', 'ava.thomas@example.com', 9890123456, '2024-06-20', 95000.00, 9000.00, 15.0),
-				('Amit Sharma','IT','amit.sharma@company.com', 9876543210, '2022-03-15', 95000.00, 15000.00, 12.5),
+				('Amit Sharma','IT','amit.sharma@company.com', 9875543210, '2022-03-15', 95000.00, 15000.00, 12.5),
 				('Pooja Nair', 'Finance', 'pooja.nair@example.com', 9678941234, '2025-10-30', 115000.00, 18000.00, 18.0),
 				('Priya Singh', 'HR', 'priya.singh@example.com', 9223454789, '2025-09-12', 70000.00, 5000.00, 10.0),
 				('Rohan Gupta', 'Sales', 'rohan.gupta@example.com', 9445678991, '2025-06-18', 88000.00, 12000.00, 12.5);
@@ -98,7 +99,7 @@ select department, count(*) as Employee_Count, round(avg(salary), 2) as Average_
 		min(salary) as Minimum_Salary, max(salary) as Maximum_Salary
 from employees
 group by department
-order by Average_Salary desc;
+order by Average_Salary desc, department;
 
 -------------------------------------------------------------------------------------------------------------------------
 -------------------------------------------------------------------------------------------------------------------------
@@ -113,7 +114,7 @@ order by Average_Salary desc;
 select employee_id, name, department, joining_date
 from employees
 where joining_date >= (current_date - interval '6 months')
-order by joining_date desc;
+order by joining_date desc, name;
 
 
 -- b) Group employees by department and count how many employees each has.
@@ -121,7 +122,7 @@ order by joining_date desc;
 select department, count(*) as employee_count
 from employees
 group by department
-order by employee_count desc;
+order by employee_count desc, department;
 
 
 -- c) Find the department with the highest average salary.
@@ -148,6 +149,7 @@ where rank = 1;
 
 -- d) Identify employees who have the same salary as at least one other employee.
 
+--v1
 select employee_id, name, department, salary
 from employees
 where salary in (
@@ -159,7 +161,15 @@ where salary in (
 order by salary desc, name;
 
 
-
+--v2
+select employee_id, name, department, salary
+from (
+		select employee_id, name, department, salary,
+				count(*) over (partition by salary) as sal_match
+		from employees
+) t
+where sal_match > 1
+order by salary desc, name;
 
 
 
